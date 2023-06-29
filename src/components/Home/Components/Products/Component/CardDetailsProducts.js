d023274a20969de6dca3f45ade9297a46b5dd1dc
tintin <michel.almont@gmail.com>
2023-06-28 20:24:14 -0400

    details en cours

---
+++ src/components/Home/Components/Products/Component/CardDetailsProducts.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export default function CardDetailsProducts() {
+  return (
+    <Wrapper></Wrapper>
+  )
+}
+const Wrapper=styled.div `
+
+`;
--- src/components/Home/Components/Products/DetailsProducts.js
+++ src/components/Home/Components/Products/DetailsProducts.js
@@ -1,63 +1,251 @@
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
-import {addToCart} from '../../../../redux/cartSlice';
-import { useDispatch } from 'react-redux';
-export default function DetailsProducts({route}) {
-    const { state } = useLocation();
-    const dispatch = useDispatch()
-    console.log(state);
+import { addToCart } from "../../../../redux/cartSlice";
+import { useDispatch } from "react-redux";
+import { HiCash } from "react-icons/hi";
+import { AiFillStar } from "react-icons/ai";
+import { IoIosPaperPlane } from "react-icons/io";
+import { BiAnalyse } from "react-icons/bi";
+import { BsChevronDown } from "react-icons/bs";
+export default function DetailsProducts({ route }) {
+  const { state } = useLocation();
+  const dispatch = useDispatch();
+  console.log(state);
   return (
-
     <Wrapper>
-  
-        <div className=" principalDetails ">
-          
-          
-            <div className="row ">
-            <div className="col  m-4 right ">
-            <img className="imageEncadrement " width={530} src={state.image} alt="" />
-            </div>
-            <div className="col center">
+      <div className=" principalDetails ">
+        <div className="row ">
+          <div className="col  m-4 right ">
+            <img
+              className="imageEncadrement "
+              width={530}
+              src={state.image}
+              alt=""
+            />
+          </div>
+          <div className="col center">
             <h1>{state.title}</h1>
             <h2>{state.price} $</h2>
-            <p>{state.description}</p>
-            <button className="btn btn-primary p-2 m-2 activeButton"
-            onClick={() => 
-            dispatch(addToCart(
-                state.id, state.title, state.image, state.price
-            ))
-            }>Add to Cart
-            </button>
-            </div>
-            </div>
-            </div>
-         
 
+            <select
+              className="form-select form-select-sm dropddownSize my-4 "
+              aria-label=".form-select-sm example"
+            >
+              <option selected>S</option>
+              <option value="2">M</option>
+              <option value="3">L</option>
+            </select>
+            <button
+              className="btn btn-primary p-2 my-4 activeButton"
+              onClick={() =>
+                dispatch(
+                  addToCart(state.id, state.title, state.image, state.price)
+                )
+              }
+            >
+              Add to Cart
+            </button>
+
+            <div className="Feature">
+              <div className="objectFeature">
+                <HiCash size={30} />
+                <p>Secure checkouts</p>
+              </div>
+              <div className="objectFeature">
+                <AiFillStar size={30} />
+                <p>fast Shipping</p>
+              </div>
+              <div className="objectFeature">
+                <IoIosPaperPlane size={30} />
+                <p>fast Shipping</p>
+              </div>
+              <div className="objectFeature">
+                <BiAnalyse size={30} />
+                <p>Easy returns</p>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>
+                <a
+                  className="btn  my-2 Collapse"
+                  data-bs-toggle="collapse"
+                  href="#multiCollapseExample1"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="multiCollapseExample1"
+                >
+                  DETAILS
+                  <BsChevronDown size={20} />
+                </a>
+              </p>
+              <div class="row">
+                <div class="col">
+                  <div
+                    class="collapse multi-collapse"
+                    id="multiCollapseExample1"
+                  >
+                    <div class="card card-body cardWith">
+                      <p>{state.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>
+                <a
+                  className="btn  my-2 Collapse"
+                  data-bs-toggle="collapse"
+                  href="#multiCollapseExample2"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="multiCollapseExample2"
+                >
+                  DELIVERY
+                  <BsChevronDown size={20} />
+                </a>
+              </p>
+              <div class="row">
+                <div class="col">
+                  <div
+                    class="collapse multi-collapse"
+                    id="multiCollapseExample2"
+                  >
+                    <div class="card card-body cardWith">
+                      USA:
+                      <br />
+                      <br />
+                      <p>Standard Shipping: 3-5 business days Express</p>
+                      <p>Shipping: 1-2 business days International: Standard</p>
+                      <br />
+                      <p>Shipping: 1-2 business days International: Standard</p>
+                      <p>
+                        Shipping: 3-5 business days Express Shipping: 1-2
+                        business
+                      </p>
+                      <br />
+                      days For detailed information on our shipping & delivery
+                      times, please visit the tab on the bottom of our web page.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>
+                <a
+                  className="btn  my-2 Collapse"
+                  data-bs-toggle="collapse"
+                  href="#multiCollapseExample3"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="multiCollapseExample3"
+                >
+                  RETURN POLICY
+                  <BsChevronDown size={20} />
+                </a>
+              </p>
+              <div class="row">
+                <div class="col">
+                  <div
+                    class="collapse multi-collapse"
+                    id="multiCollapseExample3"
+                  >
+                    <div class="card card-body cardWith">
+                      Returns Policy 
+                      <br />
+                      <br />
+                      <p>You may return your most new, unworn items
+                      within 14 days from the day you receive your package. This
+                      policy begins on the initial delivery attempt. Tags must
+                      be attached. We will not honor your return without a tag.
+                      Once we receive your package, we will inspect it and issue
+                      you an online credit (minus shipping fees.) We do not
+                      offer money back refunds.</p> 
+                      <br />
+                      ACCESSORIES, INTIMATES, AND SALE
+                      OR DISCOUNTED ITEMS ARE FINAL SALE. NO RETURNS OR EXCHANGES. 
+                      <br />
+                      <p>Online orders will not be honored in store for
+                      exchange or in-store credit. Online returns will be issued
+                      an online credit.</p>
+                       For more detailed information on our
+                      return policy or terms and conditions please visit our
+                      return policy page.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <hr />
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
+  );
 }
-const Wrapper=styled.div `
-.backg{
+const Wrapper = styled.div`
+  .backg {
     background-color: black;
-}
-.imageEncadrement{
+  }
+  .imageEncadrement {
     border-style: solid;
     border-width: 1px;
     border-color: black;
     padding: 10px;
     border-radius: 10px;
-   
-   
-}
-.center{
+  }
+  .center {
     text-align: start;
     margin-top: auto;
     margin-bottom: auto;
-}
+  }
 
-.right{
+  .right {
     text-align: end;
-   
-}
+  }
 
+  .dropddownSize {
+    width: 420px;
+  }
+  .activeButton {
+    width: 420px;
+  }
+  .Description {
+    width: 420px;
+  }
+  .Feature {
+    padding: 20px;
+    width: 420px;
+    background-color: rgba(128, 128, 128, 0.1);
+  }
+  .objectFeature {
+    margin-right: 20px;
+    display: inline-block;
+    width: 70px;
+    height: 80px;
+    text-align: center;
+  }
+  .objectFeature p {
+    margin-top: 10px;
+    text-align: center;
+    font-size: 12px;
+  }
+  .cardWith {
+    width: 420px;
+  }
+  hr {
+    margin-top: 0px;
+    width: 420px;
+  }
+  .Collapse {
+    display: flex;
+    justify-content: space-between;
+    width: 420px;
+    font-size: 12px;
+  }
 `;
